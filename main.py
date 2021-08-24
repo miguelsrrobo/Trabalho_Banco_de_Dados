c2cb17d7c1306790a3acc8c92c3f7b4fe7797943
MiguelGSilvestre <54369110+miguelsrrobo@users.noreply.github.com>
2021-08-24 11:38:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from connection import connection, close_connection
 from operantion_table import select_element_in_table
 
 def main():
-    conn = connection( "172.17.0.2", "root", "root", "buteco")
+    conn = connection( "******", "root", "****", "buteco")
 
     insert_into_table(conn, "2", "", "24516783", "meuquiadis bonifasio espindula", "")
 
